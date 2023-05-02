739ae2412a31223b0311889b9f03c4249c42872a
jh0152park <jh0152park@naver.com>
2023-05-02 23:12:05 +0900

    run flask web server to gathering job lists from indeed and wwr site

--- jh0152park/job_parser.py
+++ jh0152park/job_parser.py
@@ -1,6 +1,10 @@
 import csv
+from flask import *
 from extractor.wwr_parser import *
 from extractor.indeed_parser import *
+
+
+DB = {}
 
 
 class Writer:
@@ -18,15 +22,46 @@ class Writer:
         ]
         with open(f"{self.skill}_job_list.csv", "w", encoding="utf-8-sig", newline="") as f:
             writer = csv.writer(f)
+            writer.writerow(agenda)
             for job in self.jobs:
                 writer.writerow([job[j]for j in agenda])
 
 
 def main():
-    skill = "python"
-    jobs = get_wwr_job_list(skill) + get_indeed_job_list(skill)
-    writer = Writer(skill, jobs)
-    writer.save()
+    app = Flask("JobScrapper")
+
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+
+    @app.route("/search")
+    def search():
+        global DB
+        skill = request.args.get("skill")
+        if not skill:
+            return redirect("/")
+
+        if skill in DB:
+            jobs = DB[skill]
+        else:
+            jobs = get_wwr_job_list(skill) + get_indeed_job_list(skill)
+            DB[skill] = jobs
+            writer = Writer(skill, jobs)
+            writer.save()
+        return render_template("search.html", skill=skill, jobs=jobs)
+
+    @app.route("/export")
+    def export():
+        global DB
+
+        skill = request.args.get("skill")
+        if not skill:
+            return redirect("/")
+        if skill not in DB:
+            return redirect(f"/search?skill={skill}")
+        return send_file(f"{skill}_job_list.csv", as_attachment=True)
+
+    app.run("0.0.0.0")
 
 if __name__ == "__main__":
     main()
