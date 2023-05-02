da737724b74f12caf0159786cb408474ad9e824e
jh0152park <jh0152park@naver.com>
2023-05-02 16:18:33 +0900

    extarct jobs condition from indeed site

--- jh0152park/indeed_parser.py
+++ jh0152park/indeed_parser.py
@@ -1,10 +1,41 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+
+JOB = {}
 
 URL = "https://kr.indeed.com/jobs?q="
 TERM = "python"
 
 driver = webdriver.Chrome()
-driver.get(URL)
-print(driver.page_source)
+driver.get(URL+TERM)
+
+soup = BeautifulSoup(driver.page_source, "lxml")
+pages = soup.find_all("nav", attrs={"role": "navigation"})[0].find_all("div", attrs={"class": "css-tvvxwd ecydgvn1"})
+pages = 5 if len(pages) > 5 else len(pages)
+
+for page in range(pages):
+    url = f"https://kr.indeed.com/jobs?q=python&start={page*10}"
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    jobs = soup.find_all("td", attrs={"class": "resultContent"})
+
+    for job in jobs:
+        link = job.select("h2 a")[0]["href"]
+        title = job.select("h2 a span")[0].text
+        company = job.find("span", attrs={"class": "companyName"}).text
+        type_ = "N/A" if job.find("div", attrs={"class": "metadata"}) is None else\
+                job.find("div", attrs={"class": "metadata"}).text
+
+        if company not in JOB:
+            JOB[company] = {
+                "link": "https://kr.indeed.com" + link,
+                "title": title,
+                "type": type_
+            }
+
+for company, condition in JOB.items():
+    print(f"{company}\n{condition}\n")
+
+while True:
+    pass
+
